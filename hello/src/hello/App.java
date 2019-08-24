bc54c4004bf11c066faa6bc7815b3ae8e64d9216
dell <dell@DESKTOP-8K5CIL1>
2019-08-24 16:26:44 +0800

    工程初始化

---
+++ hello/src/hello/App.java
@@ -0,0 +1,17 @@
+package hello;
+
+import com.google.gson.Gson;
+
+import okhttp3.OkHttpClient;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		
+		//	Jsoup.connect()
+		OkHttpClient client;
+		
+		Gson gson = new Gson();
+	}
+}
